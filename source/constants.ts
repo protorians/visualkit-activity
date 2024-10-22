38afb89600977da7228185f3a06940eadc260788
Protorians <protorians.dev@gmail.com>
2024-10-22 13:17:17 +0000

    Add progress type

---
+++ source/constants.ts
@@ -0,0 +1,20 @@
+export enum ProgressType {
+  Bar = 'bar',
+  Spinner = 'spinner',
+  Skeleton = 'skeleton',
+  Circle = 'circle',
+  Rotate = 'rotate',
+  Message = 'message',
+  Percent = 'percent',
+  Pulse = 'pulse',
+  Dots = 'dots',
+  Bounce = 'bounce',
+  Square = 'square',
+  Morphic = 'morphic',
+}
+
+export enum ProgressAspect {
+  Normal = 'normal',
+  Outline = 'outline',
+  Overflow = 'overflow',
+}
